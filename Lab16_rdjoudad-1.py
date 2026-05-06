1bc37f672a6cab1ef3dc647794e7041f048ed3ab
Ryma Djoudad <zenyod85@gmail.com>
2026-05-06 19:07:09 -0400

    Creating path, importing necessary packages

--- Lab16_rdjoudad-1.py
+++ Lab16_rdjoudad-1.py
@@ -1,2 +1,13 @@
 """
+Ohio Unemployment Data Mapping
+Ryma Djoudad
+Create a graph that visualizes data read from a file
+CSV file used to create line plot
+05/06/2026
 """
+
+from pathlib import Path
+import csv
+import matplotlib as plt
+
+path = Path('unit16programmingassignment/OHUR.csv')
